bb11e1b4106a91a9cc8d2bddd82ed9ef6571c0ed
becode <geoffreywislez@gmail.com>
2020-03-11 09:13:29 +0100

    Ajout Geoffrey

--- src/pages/header.php
+++ src/pages/header.php
@@ -6,7 +6,9 @@
     <title>Page d'accueil</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type='text/css'href="../SASS/CSS/style.css">
+  
+    <link rel="stylesheet" type="text/css" href="../sass/css/style.css">
+
 </head>
 <body>
 
