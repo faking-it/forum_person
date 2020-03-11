d170161fc938766ebeb39014efb07f42409d6712
Bruno <michelbruno453@hotmail.com>
2020-03-11 09:05:47 +0100

    pagination + nav onglets Bruno

--- src/pages/home.php
+++ src/pages/home.php
@@ -10,19 +10,19 @@
      
     <nav class="home">
         <ul class="nav nav-pills nav-fill">
-        <li class="nav-item">
+        <li class="nav-item All">
             <a class="nav-link active" href="#">All</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item gen">
             <a class="nav-link" href="#">General</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item dvlpt">
             <a class="nav-link" href="#">Development</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item st">
             <a class="nav-link" href="#">Small Talks</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item ev">
             <a class="nav-link" href="#">Events</a>
         </li>
         </ul>
@@ -33,26 +33,40 @@
 <div class="conteneur">
     <div class="list-group">
     <ul class="ul_topic">
+        <!-- C'est ici qu'on va rechercher les données dans la sélection exécutée dans topics_home.php -->
         <?php foreach ($topics as $topic)  {?>
-        <li class="id_topic">
+        <li class="id_topic <?php echo $topic->board_name?>">
         <a href="topic.php?id=<?php $topic->id_user ?>" class="list-group-item list-group-item-action">
             <div class="d-flex w-100 justify-content-between">
+                <div class="board"><?php echo $topic->board_name?></div>
                 <div>
-                    <img src="./images/<?php  ?>" alt="avatar" class="avatar">
+                    <img src="./images/<?php echo $topic->avatar?>" alt="avatar" class="avatar">
                 </div>
                 <div class="title"><h5><?php echo $topic->title?></h5></div>
-                <small class="date"><?php echo $topic->date_crea?></small>
+                <small class="date"><?php echo $topic->date_up?></small>
             </div>
         </a>
         </li>
         <?php } ?>
     </ul>
 
-    <a class="new_topic_button" href="./pages/new_topic.php"><button type="button" class="btn btn-primary">Créer un sujet</button></a>
+    <!-- Pagination -->
+    <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+        <?php $nbr_pages=$nbr_lignes/5;for ($i=0;$i<$nbr_pages;$i++){?>
+        <div class="btn-group mr-2" role="group" aria-label="First group">
+            <button type="button" class="btn btn-secondary <?php echo $i?>"></button>
+        </div>
+        <?php }?>
+    </div>
+
+    <!-- Créer un sujet -->
+    <div style="text-align:center;"><a class="new_topic_button" href="./pages/new_topic.php"><button type="button" class="btn btn-primary">Créer un sujet</button></a></div>
 
     </div>
 </div>
 
- </body>
- </html>
+<?php include "./js/home_script.php" ?>
+
+</body>
+</html>
 
--- src/pages/topics_home.php
+++ src/pages/topics_home.php
@@ -1,9 +1,26 @@
 <?php 
 
 require "pdo.php";
-$sql = "SELECT * FROM topics ORDER BY date_crea DESC";
+
+$sql = "SELECT date_up, title, avatar, board_id, board_name
+FROM topics 
+INNER JOIN users ON topics.user_id = users.id_user
+INNER JOIN boards ON topics.board_id = boards.id_board
+ORDER BY date_up DESC";
+
     $sth = $link->prepare($sql);
     $sth->execute();
 
     $topics = $sth->fetchAll(PDO::FETCH_OBJ);
+    $nbr_lignes = count($topics);
+
+
+
+$sql = "SELECT *
+FROM boards";
+
+    $sth = $link->prepare($sql);
+    $sth->execute();
+
+    $boards = $sth->fetchAll(PDO::FETCH_OBJ);
     
