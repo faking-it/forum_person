ba9a5cbe9e7da2f04d497abca178a2e51b17b775
becode <geoffreywislez@gmail.com>
2020-03-12 14:49:40 +0100

    Modification placement elements

--- src/pages/connexion.php
+++ src/pages/connexion.php
@@ -23,7 +23,7 @@ define("PATH", "./");
             $_SESSION['pseudo'] = $userinfo['pseudo'];
             $_SESSION['mail']   = $userinfo['mail'];
             if(isset($_SESSION["id"])){
-                header('Location: http://192.168.99.100/index.php'); 
+                header('Location: /index.php'); 
             }
             $message= "$user_pseudo . 
             <a href='affiche.php'>Cliquez ici pour voir la liste des topics</a> <br>
