a7987f5c8f618496421dd2918df610f0bb078e56
faking-it <michelbruno453@hotmail.com>
2020-03-17 17:06:53 +0100

    Ajout des onglets random et very secret

--- pages/new_topic.php
+++ pages/new_topic.php
@@ -25,23 +25,6 @@ require "../pdo.php";
                // $message = 'success';
                //header('Location: ../index.php'); 
                 $message = " Votre topic a été bien ajouté .. merci!";
-
-                // Effacer les articles en trop de l'onglet Random
-                foreach ($topics as $topic){
-                    if ($topic->board_id == 5){
-                        $topics_rdm++;
-                    }
-                }
-
-                if ($topics_rdm>5){
-                    for ($j=5;$j<$topics_rdm;$j++){
-                        $sql_delete = "DELETE FROM topics WHERE board_id = 5 ORDER BY date_crea ASC LIMIT 1";
-                        $sth = $link->prepare($sql_delete);
-                        $sth->execute();
-                        $topics = $sth->fetchAll(PDO::FETCH_OBJ);
-                    }
-                }
-
                 } else {
                  
                    // $message = 'error 404';
