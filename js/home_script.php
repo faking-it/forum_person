dc53a496f3793897c781e01530df7e900aedf85f
faking-it <michelbruno453@hotmail.com>
2020-03-17 16:43:48 +0100

    Ajout des onglets random et very secret

--- js/home_script.php
+++ js/home_script.php
@@ -203,7 +203,7 @@ document.getElementsByClassName("All")[0].addEventListener("click", () => {
             $topics_rdm++;
         }
     }
-    require "../pages/topics_home.php";
+
     if ($topics_rdm>5){
         $sql_delete = "DELETE * FROM topics WHERE board_id=5 EXCEPT SELECT TOP 5 * FROM topics WHERE board_id=5";
         $sth = $link->prepare($sql_delete);
