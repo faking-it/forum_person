0812b70b5bdb85de10fda13148005978ae35db65
faking-it <michelbruno453@hotmail.com>
2020-03-17 13:39:30 +0100

    Ajout des onglets random et very secret

--- js/home_script.php
+++ js/home_script.php
@@ -213,7 +213,7 @@ document.getElementsByClassName("All")[0].addEventListener("click", () => {
     $nodeList = $xpath->query("//div[@class='id_topic Random']");
     $node = $nodeList->item(0);
 
-    echo $node="okkkkkkkk";
+    echo "okkkkkkkk";
     ?>
             
     // Afficher les numéros de page
