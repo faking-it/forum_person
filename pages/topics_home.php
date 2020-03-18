e344c0d3e668143eb234d8c3395b1fc602898337
faking-it <michelbruno453@hotmail.com>
2020-03-18 10:17:53 +0100

    Ajout des onglets random et very secret

--- pages/topics_home.php
+++ pages/topics_home.php
@@ -23,22 +23,3 @@ FROM boards";
     $sth->execute();
 
     $boards = $sth->fetchAll(PDO::FETCH_OBJ);
-
-// Effacer les articles en trop de l'onglet Random
-
-foreach ($topics as $topic){
-    if ($topic->board_id == 5){
-        $topics_rdm++;
-        echo $topics_rdm;
-    }
-}
-echo $topics_rdm;
-if ($topics_rdm>5){
-    for ($j=5;$j<$topics_rdm;$j++){
-        $sql_delete = "DELETE FROM topics WHERE board_id = 5 ORDER BY date_crea ASC LIMIT 1";
-        $sth = $link->prepare($sql_delete);
-        $sth->execute();
-        $topics = $sth->fetchAll(PDO::FETCH_OBJ);
-    }
-}
-?>
